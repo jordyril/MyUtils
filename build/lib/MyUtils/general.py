fdca689420f80a7ecac451383fadbebba19ae660
jordy_bf.uzh <jordy.rillaerts@bf.uzh.ch>
2020-06-25 11:33:30 +0200

    imported missing os and numpy

--- build/lib/MyUtils/general.py
+++ build/lib/MyUtils/general.py
@@ -4,6 +4,8 @@
 import logging
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import numpy as np
 # =============================================================================
 # support functions
 # =============================================================================
